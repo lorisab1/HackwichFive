93c5446227927cb767ab7f1d71ff4fc435c29558
Lorisa Bulosan <lorisab@hawaii.edu>
2023-09-28 14:13:10 -1000

    Completed Problem Set for Tab 2

--- HackwichFive/secondViewController.swift
+++ HackwichFive/secondViewController.swift
@@ -9,13 +9,31 @@ import UIKit
 
 class secondViewController: UIViewController {
 
+   var currentIndex = 0
+    let favoriteDrinksArray = ["Boba Tea", "Iced Coffee", "Matcha Latte", "Apple Juice", "Peach Ice Tea"]
+    
+    @IBOutlet weak var topLabel: UILabel!
+    
+   
+    @IBOutlet weak var bottomLabel: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-    
+        topLabel.text = "My Favorite Drinks:"
+        
     }
     
 
-   
+    @IBAction func buttonPressed(_ sender: Any)
+    {
+        
+        bottomLabel.text = favoriteDrinksArray[2]
+        
+    }
+ 
+    
+    
 
 }
