52b16d23d369140cab6169ee944762264388f7d4
Lorisa Bulosan <lorisab@hawaii.edu>
2023-10-05 15:15:15 -1000

    Completed Problems Sets for Part 2

--- HackwichFive/ViewController.swift
+++ HackwichFive/ViewController.swift
@@ -30,23 +30,27 @@ class ViewController: UIViewController {
     
     @IBAction func buttonPressed(_ sender: Any)
     {
+        //Comment #1 Line 34: Conditional statement - currentIndex starts at 0, favoriteFoodsArray has 5 items, index is 4
         if currentIndex < favoriteFoodsArray.count
         {
+            //Comment #2 Line 37: Code for cycling through the array
             bottomLabel.text = favoriteFoodsArray[currentIndex]
-            currentIndex = currentIndex + 1
             
-            
+            //Comment #3 Line 40: Changes the button label to say "Next"
             buttonLabel.setTitle("Next", for: UIControl.State.normal)
+            
+            //Comment #4 Line 43: Accesses the next element in the array
             currentIndex += 1
             
         }
+        
+        //Comment #5 Line 48-51: else statement, print lets the program know there is no more items in the array
         else
         {
-            print("No more elements to display.")
-            
-            
+        
             print("button has been disabled")
             
+            //Comment #6 Line 54: Disables button (grayed out)
             (buttonLabel!).isEnabled=false
             
         }
--- HackwichFive/secondViewController.swift
+++ HackwichFive/secondViewController.swift
@@ -25,30 +25,40 @@ class secondViewController: UIViewController {
         super.viewDidLoad()
 
         topLabel.text = "My Favorite Drinks:"
-        
     }
     
 
     @IBAction func buttonPressed(_ sender: Any)
     {
+        //Comment #1 Line 34: Conditional statement - currentIndex starts at 0, favoriteDrinksArray has 5 items, index is 4
         if currentIndex < favoriteDrinksArray.count
         {
+            //Comment #2 Line 37: Code for cycling through the array
             bottomLabel.text = favoriteDrinksArray[currentIndex]
-            currentIndex = currentIndex + 1
             
+            //Comment #3 Line 40: Changes the button label to say "Next"
             buttonLabel.setTitle("Next", for: UIControl.State.normal)
+           
+            //Comment #4 Line 43: Accesses the next element in the array
             currentIndex += 1
             
         }
+       
+        //Comment #5 Line 48-51: else statement, print lets the program know there is no more items in the array
         else
         {
-            print("No more elements to display.")
+            
+            print("button has been disabled")
+            
+            //Comment #6 Line 54: Disables button (grayed out)
+            (buttonLabel!).isEnabled=false
             
         }
+       
         
-   
+        
     }
- 
+
     
     
 }
